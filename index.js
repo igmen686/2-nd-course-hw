7f9a83244c2a3e9f5ea8a2760f5cd0a4c49594d0
Eligah <elijahpus@gmail.com>
2023-09-26 21:37:36 +0700

    +++

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* Домашняя работа 2*/
+/*
 //1
 let t = 10;
 t = 20;
@@ -58,5 +60,86 @@ alert (user2[info]);
 //10
 let nameUser = prompt('Введите ваше имя');
 alert(`Привет ${nameUser}!`);
+*/
 
+/* Домашняя работа 3*/
+/*
+//1
+let password = 'super_password123';
+let input = prompt('Введите пароль')
+    switch (input) {
+        case (password):
+            alert('Пароль введен верно');
+            break;
+    
+        default: alert('Пароль введен неправильно');
+            break;
+    }
 
+//2
+let c = 5;
+    if (c>0 && c<10) {
+        console.log ('Верно');
+    } else {
+        console.log ('Неверно');
+    }
+
+//3
+let d = 5;
+let e = 105;
+    if (d>100 || e>100) {
+        console.log ('Верно');
+    } else {
+        console.log ('Неверно');
+    }
+
+//4
+let a = '2';
+let b = '3';
+let f = Number(a);
+let g = Number(b);
+alert (f + g);
+
+//5
+let monthNumber = Number(prompt('Введите порядковый номер месяца, чтобы узнать к какому сезону он относится'));
+    switch (monthNumber) {
+        case 12:
+            alert('Зима');
+            break;
+        case 1:
+            alert('Зима');
+            break;
+        case 2:
+            alert('Зима');
+            break;
+        case 3:
+            alert('Весна');
+            break;
+        case 4:
+            alert('Весна');
+            break;
+        case 5:
+            alert('Весна');
+            break;
+        case 6:
+            alert('Лето');
+            break;
+        case 7:
+            alert('Лето');
+            break;
+        case 8:
+            alert('Лето');
+            break;
+        case 9:
+            alert('Осень');
+            break;
+        case 10:
+            alert('Осень');
+            break;
+        case 11:
+            alert('Осень');
+            break;
+        default: alert('Конец света');
+            break;
+    }
+*/
